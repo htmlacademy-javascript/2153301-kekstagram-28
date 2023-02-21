0ddf42f89c812b464a23fa82c90dd0e836eca76d
evgeny shlyk <serpupstars@gmail.com>
2023-02-21 17:06:03 +1000

    первый комиит

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+ function checkLengthString (string) {
+ if (string <= 20) {
+   console.log('правильно');
+ } else {
+   console.log('неправильно');
+ }
+}
+string = 20
+
+ checkLengthString(30)
+
