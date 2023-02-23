3992840f1393df5e941ab5d2f46e7924e3feb128
Evgene Shlyk <serpupstars@gmail.com>
2023-02-24 07:16:21 +1000

    добавляет функции на первую проверку

--- js/functions.js
+++ js/functions.js
@@ -7,19 +7,37 @@
    return str === str.split('').reverse().join('');
  }
 
-function findAllNumber (str) {
-  return str.replace(/[^0-9]/g,'');
-}
-// function generationAddress (iStr, time, addStr ) {
-//   let address =
-// }
+ let resultNumber = '';
+ let extractNumber = (string) => {
+   for (let i = 0; i < string.length; i++) {
+     if (!Number.isNaN(parseInt(string.at(i), 10))) {
+       resultNumber += string.at(i);
+     }
+   }
+ };
 
 
 
- //console.log(findAllNumber('ECMAScript 2022'));
+ // Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
 
- // console.log(checkPalindrome ('шалаш'));
+ const leftPad = (text, minLength, addCharacters) => {
+   const inputText = text.toString();
+   // приводим text к строке и пишем в переменную inputText
+   const inputAddCharacters = addCharacters.toString();
+   // приводим addCharacters к строке и пишем в переменную inputAddCharacters
+   if(inputText.length < minLength) {
+     const outputAddCharaters = inputAddCharacters.repeat(minLength - inputText.length);
+     // повторяем добавочную строку (minLength-inputText.length) раз
+     return outputAddCharaters.slice(0, minLength - inputText.length) + inputText;
+     /* возвращаем массив outputAddCharaters от 0 до (minLength-inputText.length)
+     и добавляем введенный текст если текст меньше указанной длины
+     */
+   }
+   return inputText;
+   // возвращаем введенный текст если он больше указанной длины
+ };
 
- // console.log(checkLengthLine(30));
+
+
 
 
