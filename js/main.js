fa2c4891d31d4adf821cbec4190dab895ee6fda5
evgeny shlyk <serpupstars@gmail.com>
2023-03-07 16:43:16 +1000

    начинает дз-4

---
+++ js/main.js
@@ -0,0 +1,80 @@
+{
+  const ID = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25
+  ];
+  const URL = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25
+  ];
+  description
+  likes
+  comments
+  avatar
+}
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const createObject = () => {
+  return {
+
+  }
+}
+
+
+Array.from(Array(10).keys());
+
+console.log(Array.from(Array(10).keys()));
