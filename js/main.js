081ae9fd771345c84ce683fb037ade60442a7867
Evgene Shlyk <serpupstars@gmail.com>
2023-03-08 17:38:33 +1000

    lint ДЗ-4

--- js/main.js
+++ js/main.js
@@ -64,25 +64,19 @@ const getRandomInteger = (a, b) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const createObject = () => {
-  return {
+const createObject = () => (
+  {
     id: getRandomArrayElement(ID),
-    url: (
-      'photos/' + getRandomArrayElement(URL) + '.jpg'
-    ),
+    url: `photos/${getRandomArrayElement(URL)}.jpg`,
     description: getRandomArrayElement(DESCRIPTION),
     likes: getRandomArrayElement(LIKES),
-    avatar: (
-      'img/avatar-' + getRandomArrayElement(AVATAR) + '.svg'
-    ),
+    avatar: `img/avatar-${getRandomArrayElement(AVATAR)}.svg`,
     message: getRandomArrayElement(COMMENTS),
     messageId: getRandomArrayElement(MESSAGE_ID),
     name: getRandomArrayElement(NAME)
-  };
-};
+  }
+);
 
 const similarObject = Array.from({ length: QUANTITY_PHOTOS }, createObject);
 
-console.log(similarObject);
-
-
+// console.log(similarObject);
