28451f23ebcbc1f674725e06b51dd96c572197f8
evgeny shlyk <serpupstars@gmail.com>
2023-03-22 13:03:23 +1000

    проверка 22-03

--- js/full-screen.js
+++ js/full-screen.js
@@ -1,6 +1,15 @@
 
-// section Полноэкранного показа изображения.
+// контейнер полноэкранного показа изображения.
 const bigPicture = document.querySelector('.big-picture');
+// маленькое фото
+const smallPicture = document.querySelector('.picture__img');
+// IMG полноэкранного показа изображения
+// const bigPictureImg = bigPicture.querySelector('.big-picture__img');
+
+// отслеживание клика на маленькое фото
+smallPicture.addEventListener('click', () => {
+  bigPicture.classList.remove('hidden');
+});
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-import './util.js';
+// import './util.js';
 import './full-screen.js';
 
 
 import { createObjects } from './constants.js';
 import { renderThumbnails } from './drawing-thumbnails.js';
+// import {logger} from 'browser-sync/dist/logger.js';
 
 renderThumbnails(createObjects());
+
+
 
 
