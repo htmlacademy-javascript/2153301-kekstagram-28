2d9a53083cf25d4dd1d668de2eb62eb8732cb36c
Evgene Shlyk <serpupstars@gmail.com>
2023-03-25 18:32:42 +1000

    проверка 25

--- js/displayBigPicture.js
+++ js/displayBigPicture.js
@@ -1,6 +1,4 @@
-// import {isEnterKey} from './util.js';
-
-
+import {isEnterKey} from './util.js';
 
 const container = document.querySelector('.pictures');
 const bigPictureWrap = document.querySelector('.big-picture');
@@ -8,26 +6,39 @@ const bigPictureImg = bigPictureWrap.querySelector('.big-picture__img img');
 const likesCount = bigPictureWrap.querySelector('.likes-count');
 
 
-
-
 container.addEventListener('click', (evt) => {
-  const thumbnail = evt.target.closest('[data-thumbnail-id]');
-  
-
-
-  if (thumbnail) {
-    showBigPicture();}
-  bigPictureImg.src = thumbnail.src;
-  // likesCount.textContent =
+  debugger;
+  if (evt.target.closest('[data-thumbnail-id]')) {
+    // const pictureCommentsCount = evt.target.querySelector('')
+    showBigPicture();
+    bigPictureImg.src = evt.target.src;
+  }
 });
+
 
 container.addEventListener('keydown', (evt) => {
-  const thumbnail = evt.target.closest('[data-thumbnail-id]');
-  if (thumbnail) {
-    showBigPicture()}
+
+  if (isEnterKey(evt)) {
+  const imgLink = evt.target.closest('[data-thumbnail-id]');
+
+    if (imgLink) {
+      debugger;
+      const imgInsideLink = imgLink.querySelector('.picture__img');
+
+      showBigPicture();
+      bigPictureImg.src = imgInsideLink.src;
+    }
+  }
 });
+
 
 let showBigPicture = () => {
   bigPictureWrap.classList.remove('hidden');
-
 };
+
+
+
+
+
+
+
