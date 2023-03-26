85924f550d61cdbb24bc6fa5fefd85b920ee4d4b
Keks <keks@htmlacademy.ru>
2023-03-26 22:50:07 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,18 @@
+// import './gallery.js'
+// import  './displayBigPicture.js'
+
+
+import { createObjects } from './constants.js';
+import { renderThumbnails } from './create-thumbnails.js';
+import { addHandlers } from './displayBigPicture2.js';
+
+
+const objects = createObjects();
+
+
+renderThumbnails(objects);
+
+// console.log(renderThumbnails);
+
+addHandlers(objects);
+
