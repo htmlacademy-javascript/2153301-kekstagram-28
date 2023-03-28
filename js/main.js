29f70cd7f4918f2878e36d750bd9ef0a89e17a5a
Evgene Shlyk <serpupstars@gmail.com>
2023-03-29 07:16:18 +1000

    29 утро

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 import { createObjects } from './constants.js';
 import { renderThumbnails } from './create-thumbnails.js';
 import { addHandlers } from './displayBigPicture2.js';
+import './user-form.js'
 
 
 const objects = createObjects();
 
 
 renderThumbnails(objects);
-
-// console.log(renderThumbnails);
-
 addHandlers(objects);
+
+
+
 
--- js/user-form.js
+++ js/user-form.js
@@ -1,13 +1,27 @@
 const formUploadImg = document.querySelector('.img-upload__form');
 const inputUploadFile = document.querySelector('#upload-file');
-const blockImgOverlay = document.querySelector('.img-upload__overlay');
+// модальное окно редактирования фотографии
+const imgOverlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
+// кнопка закрытия окна редактирования фотографии
+const uploadCancel = document.querySelector('#upload-cancel');
 
-const pristine = new Pristine(formUploadImg);
+
+const showPhotoEditing = () => {
+
+};
 
 inputUploadFile.addEventListener('change', (evt) => {
-  console.log('привет');
-  blockImgOverlay.classList.remove('hidden');
+  imgOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
 })
+
+
+const closePhotoEditing = () => {
+  uploadCancel.addEventListener('click', () => {
+    imgOverlay.classList.add('hidden');
+    body.classList.remove('modal-open');
+  });
+
+}
 
