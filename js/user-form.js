c123674c667bdf6e9b4311a1a0942e227a209654
Evgene Shlyk <serpupstars@gmail.com>
2023-03-30 07:18:04 +1000

    30 утро

--- js/user-form.js
+++ js/user-form.js
@@ -6,6 +6,34 @@ const imgOverlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
 // кнопка закрытия окна редактирования фотографии
 const uploadCancel = document.querySelector('#upload-cancel');
+// валидируемая форма
+const uploadForm = document.querySelector('.img-upload__form');
+
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+});
+
+function validateComment (value) {
+  return value <= 140;
+}
+
+function validateHashtags (value) {
+  return value = /^#[a-zа-яё0-9]{1,19}$/i;
+}
+
+pristine.addValidator(uploadForm.querySelector('#text-description'), validateComment, 'Не более 140 символов');
+pristine.addValidator(uploadForm.querySelector('#text-hashtags'), validateHashtags, 'Хэштэг должен содержать только буквы и цифры и должен быть не менее двух и не более 20 знаков')
+
+
+uploadForm.addEventListener('submit', function (evt)  {
+  validateComment = pristine.validate();
+  // evt.preventDefault();
+});
 
 const handleEscapeKeydown = (evt) => {
   if(isEscapeKey(evt)) {
@@ -22,6 +50,7 @@ const closePhotoEditing = () => {
     imgOverlay.classList.add('hidden');
     body.classList.remove('modal-open');
     inputUploadFile.reset();
+    pristine.reset();
     document.removeEventListener('keydown', handleEscapeKeydown);
     uploadCancel.removeEventListener('click', closePhotoEditing)
   };
