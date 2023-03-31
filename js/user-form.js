e3a06564ad4ca5b26e4c1adc6da341c8c02ef3af
evgeny shlyk <serpupstars@gmail.com>
2023-03-31 17:38:50 +1000

    дз 9.14

--- js/user-form.js
+++ js/user-form.js
@@ -10,6 +10,10 @@ const uploadCancel = document.querySelector('#upload-cancel');
 // валидируемая форма
 const uploadForm = document.querySelector('.img-upload__form');
 
+// поля комментариев и хэштегов
+const commentField = uploadForm.querySelector('#text-description');
+const hashtagsField = uploadForm.querySelector('#text-hashtags');
+
 const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorClass: 'form__item--invalid',
@@ -18,40 +22,33 @@ const pristine = new Pristine(uploadForm, {
   errorTextTag: 'span',
   errorTextClass: 'form__error'
 });
-
+// валидация поля комментариев
 function validateComment (value) {
   return value.length <= 140;
 }
-
-// const validateHashtags = (value) => {
-//   const valueFreeSpace = value.trimEnd();
-//
-//   if(valueFreeSpace.length === 0) {
-//     return true;
-//   } else {
-//     const splittedValue = valueFreeSpace.split(' ');
-//     return splittedValue.every((item) =>
-//       /^#[a-zа-яё0-9]{1,19}$/i.test(item));
-//   }
-// };
-
+// проверка на повторения хэштегов
+const hasUniqueTags = (tags) => {
+  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+}
+// валидация поля хэштегов
 const validateHashtags = (value) => {
-  const valueFreeSpace = value.trimEnd();
-
-  if(valueFreeSpace.length === 0) {
-    return true;
-  } else {
-    const splittedValue = valueFreeSpace.split(' ');
-    if (splittedValue.length < 6) {
-      return splittedValue.every((item) =>
-        /^#[a-zа-яё0-9]{1,19}$/i.test(item));
+    const valueFreeSpace = value.trimEnd();
+    if(valueFreeSpace.length === 0) {
+      return true;
+    } else {
+      const splittedValue = valueFreeSpace.split(' ');
+      if (splittedValue.length < 6) {
+        return splittedValue.every((item) =>
+          /^#[a-zа-яё0-9]{1,19}$/i.test(item)) && hasUniqueTags(splittedValue);
+      }
     }
-  }
 };
 
-pristine.addValidator(uploadForm.querySelector('#text-description'), validateComment, 'Не более 140 символов');
-pristine.addValidator(uploadForm.querySelector('#text-hashtags'), validateHashtags, 'Хэштэг должен начинаться с' +
-  ' #,содержать буквы, цифры и должен быть не менее двух и не более 20 знаков. Нельзя использовать более пяти хэш-тегов.');
+pristine.addValidator(commentField, validateComment, 'Не более 140 символов');
+pristine.addValidator(hashtagsField, validateHashtags, 'Хэштэг должен начинаться с' +
+  ' #,содержать буквы, цифры и должен быть не менее двух и не более 20 знаков. Нельзя использовать более пяти' +
+  ' хэш-тегов. Нельзя использовать два одинаковых хэш тега.');
 
 uploadForm.addEventListener('submit', function (evt)  {
   if(!pristine.validate()) {
@@ -70,21 +67,30 @@ const showPhotoEditing = () => {
   body.classList.add('modal-open');
 };
 
+const removeListenerField = (evt) => {
+  if(isEscapeKey(evt)) {
+    evt.stopPropagation();
+  }
+}
+
 const closePhotoEditing = () => {
   imgOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
   inputUploadFile.reset();
   pristine.reset();
   document.removeEventListener('keydown', handleEscapeKeydown);
-  uploadCancel.removeEventListener('click', closePhotoEditing)
-
+  uploadCancel.removeEventListener('click', closePhotoEditing);
+  commentField.removeEventListener('keydown', removeListenerField);
+  hashtagsField.removeEventListener('keydown', removeListenerField);
 };
 
 export const handleUserForm = () => {
   inputUploadFile.addEventListener('change', () => {
     showPhotoEditing();
     document.addEventListener('keydown', handleEscapeKeydown);
-    uploadCancel.addEventListener('click', closePhotoEditing)
+    uploadCancel.addEventListener('click', closePhotoEditing);
+    commentField.addEventListener('keydown', removeListenerField);
+    hashtagsField.addEventListener('keydown', removeListenerField) ;
   });
 };
 
