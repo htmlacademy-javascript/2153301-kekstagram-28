65ef3c310909f8bb2a868cc8b01c50177d5b9339
Keks <keks@htmlacademy.ru>
2023-03-31 10:41:35 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,15 @@
+import { createObjects } from './constants.js';
+import { renderThumbnails } from './create-thumbnails.js';
+import { addHandlers } from './displayBigPicture.js';
+import { handleUserForm } from './user-form.js';
+
+
+const objects = createObjects();
+
+
+renderThumbnails(objects);
+addHandlers(objects);
+handleUserForm();
+
+
+
