f181b019c69a73a0204e5732f5a4829586d0b25b
Keks <keks@htmlacademy.ru>
2023-04-07 06:22:05 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/main.js
@@ -0,0 +1,20 @@
+import { renderThumbnails } from './create-thumbnails.js';
+import { addHandlers } from './displayBigPicture.js';
+import { handleUserForm, setUserFormSubmit, closePhotoEditing} from './user-form.js';
+import { getData} from './api.js';
+import {showAlert} from './util.js';
+import './modal-error-success.js';
+
+getData()
+  .then((photos) => {
+    renderThumbnails(photos);
+    handleUserForm(photos);
+    addHandlers(photos);
+  })
+  .catch(
+    (err) => {
+      showAlert(err.message);
+    }
+  );
+
+setUserFormSubmit(closePhotoEditing);
